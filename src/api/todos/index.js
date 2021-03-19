f7f56118e8efe8d6d37fbff6157daed1c27a9abb
saseungmin <dbd02169@naver.com>
2021-03-19 21:07:28 +0900

    [Feature] Create multiple remove api
    - Create multipleRemove Api and checkTodoByIds middleware

--- src/api/todos/index.js
+++ src/api/todos/index.js
@@ -1,13 +1,14 @@
 import Router from 'koa-router';
 
 import {
-  write, list, remove, getTodoById, update, checkOwnTodo,
+  write, list, remove, getTodoById, update, checkOwnTodo, multipleRemove, checkTodoByIds,
 } from './todos.ctrl';
 
 const todos = new Router();
 
 todos.get('/', list);
 todos.post('/', write);
+todos.delete('/', checkTodoByIds, multipleRemove);
 
 const todo = new Router();
 
